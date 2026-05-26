c4dd6c17d67f53e1f6bd90e71caf4ed0b3cbc5ea
Alexihax <Alexihax@gmail.com>
2026-05-26 18:22:35 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,90 @@
-loadstring(game:HttpGet(
-"https://raw.githubusercontent.com/Alexihax/Alexihax-HUB/main/main.lua"
-))()
+--// =========================================
+--// PLATOBOOST LOADER
+--// =========================================
+
+local HttpService =
+    game:GetService("HttpService")
+
+local Players =
+    game:GetService("Players")
+
+local player =
+    Players.LocalPlayer
+
+-- =====================================
+-- CONFIG
+-- =====================================
+
+local SERVICE = 25623
+
+-- =====================================
+-- HWID
+-- =====================================
+
+local HWID =
+    game:GetService("RbxAnalyticsService")
+    :GetClientId()
+
+-- =====================================
+-- KEY FILE
+-- =====================================
+
+local KEY_FILE = "AlexihaxKey.txt"
+
+-- =====================================
+-- READ KEY
+-- =====================================
+
+local key = nil
+
+if isfile(KEY_FILE) then
+
+    key = readfile(KEY_FILE)
+end
+
+-- =====================================
+-- ASK FOR KEY
+-- =====================================
+
+if not key or key == "" then
+
+    key = game:GetService("StarterGui")
+        :PromptTextInput(
+            "Enter Key",
+            "Paste Platoboost Key"
+        )
+end
+
+-- =====================================
+-- VERIFY
+-- =====================================
+
+local url =
+    "https://api.platoboost.com/public/whitelist/"
+    .. SERVICE
+    .. "?identifier="
+    .. HWID
+    .. "&key="
+    .. key
+
+local response =
+    game:HttpGet(url)
+
+local data =
+    HttpService:JSONDecode(response)
+
+if data.success
+and data.data
+and data.data.valid then
+
+    writefile(KEY_FILE, key)
+
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/Alexihax/Alexihax-HUB/main/main.lua"
+    ))()
+
+else
+
+    warn("Invalid Key")
+
+end
